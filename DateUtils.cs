71707ded2db23c06ea1b38ded763c57f9f1254cc
Unknown <luungoc2005@gmail.com>
2017-07-10 10:36:02 +0700

    Add business day functions

--- DateUtils.cs
+++ DateUtils.cs
@@ -41,5 +41,65 @@ namespace DotNetUtils
         {
             return (new DateTime(1970, 1, 1)).AddSeconds(value);
         }
+
+        /// <summary>
+        /// Add x number of business days to supplied DateTime
+        /// </summary>
+        /// <param name="value">The date to add</param>
+        /// <param name="days">Number of days to add. Must be positive</param>
+        /// <returns></returns>
+        public static DateTime GetNextDay(DateTime value, int days)
+        {
+            DateTime retVal = value;
+            if (days < 0)
+            {
+                throw new ArgumentException("Can only add a positive number of days")
+            }
+            else if (days == 0)
+            {
+                return retVal;
+            }
+            else
+            {
+                if (retVal.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    retVal.AddDays(2); // Move forward to Monday
+                    days -= 1;
+                }
+                else if (retVal.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    retVal.AddDays(1); // Move forward to Monday
+                    days -= 1;
+                }
+
+                retVal.AddDays(days / 5 * 7);
+                int extraDays = days % 5;
+
+                if ((int)retVal.DayOfWeek + extraDays > 5)
+                {
+                    extraDays += 2;
+                }
+
+                return retVal.AddDays(extraDays);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startD"></param>
+        /// <param name="endD"></param>
+        /// <returns></returns>
+        public static double GetBusinessDays(DateTime startDate, DateTime endDate)
+        {
+            double calcBusinessDays =
+                1 + ((endD - startD).TotalDays * 5 -
+                (startD.DayOfWeek - endD.DayOfWeek) * 2) / 7;
+
+            if (endD.DayOfWeek == DayOfWeek.Saturday) calcBusinessDays--;
+            if (startD.DayOfWeek == DayOfWeek.Sunday) calcBusinessDays--;
+
+            return calcBusinessDays;
+        }
     }
 }
